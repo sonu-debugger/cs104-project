f55034c75570206ac185090987860f31cb08750e
Banothu Sonusree <sonusree.b2006@gmail.com>
2025-04-06 14:06:08 +0530

    Delete live-page directory

--- live-page/score.js
+++
@@ -1,6 +0,0 @@
-/* function 1*/
-
-/* fucntionn that shows the score*/
-/* if first innings, its like CSK 50/3 (1.4) vs. RCB and rcb won toss, choose bowl*/
-/*if second inningsm its like RCB 20/1 (0.5) vs. CSK 61/4 (2) and rcb need 42 runs to win*/
-
